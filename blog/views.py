bdb3d71054d2f1a2a47e391c16c20986b470f9d0
Sanjith0524 <sanjith.m2023@vitstudent.ac.in>
2023-12-29 16:28:29 +0530

    Final Commit

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 all_posts = [
     {
-        'slug':'hike-in-the mountains',
-        'image':'mountains.jpg',
+        'slug':'the-mountains',
+        'image':'mountains.jpeg',
         'author':'Eren',
         'date':date(2023,12,30),
         'title':'Mountain Hiking',
@@ -24,12 +24,12 @@ all_posts = [
     },
 
     {
-        'slug':'ski',
-        'image':'mountains.jpg',
+        'slug':'Skiing',
+        'image':'skiing.jpeg',
         'author':'Eren',
-        'date':date(2023,12,30),
-        'title':'Mountain Hiking',
-        'excerpt':"here's nothing like the views you get when hiking in the mountains! and I wasn't even prepared for what happened whilst I was enjoying the view!",
+        'date':date(2019,3,5),
+        'title':'Skiing',
+        'excerpt':"Alpine skiing is the sport of sliding down snow-covered hills on skis with fixed-heel bindings.Alpine skiing is the sport of sliding down snow-covered hills",
         'content':""""
         tag defines the contact information for the author/owner of a document or an article.
         The contact information can be an email address, URL, physical address, phone number,
@@ -45,11 +45,11 @@ all_posts = [
 
     {
         'slug':'Travelling',
-        'image':'mountains.jpg',
+        'image':'Travelling.jpeg',
         'author':'Eren',
-        'date':date(2023,12,30),
-        'title':'Mountain Hiking',
-        'excerpt':"here's nothing like the views you get when hiking in the mountains! and I wasn't even prepared for what happened whilst I was enjoying the view!",
+        'date':date(2021,1,30),
+        'title':'Around The World',
+        'excerpt':"Paisu Pok Lake is a beautiful blue lake in Banggai, Indonesia that was still a hidden gem until very recently beautiful blue lake in Banggai,",
         'content':""""
         tag defines the contact information for the author/owner of a document or an article.
         The contact information can be an email address, URL, physical address, phone number,
@@ -73,7 +73,8 @@ def starting_page(request):
     return render(request,'blog/index.html',{'posts':latest_post,})
 
 def posts(request):
-    return render(request,'blog/all-posts.html')
+    return render(request,'blog/all-posts.html',{'all_posts':all_posts})
 
 def post_detail(request,slug):
-    return render(request,'blog/post-detail.html')
+    identified_post = next(post for post in all_posts if post['slug'] == slug) 
+    return render(request,'blog/post-detail.html',{'post' : identified_post})
