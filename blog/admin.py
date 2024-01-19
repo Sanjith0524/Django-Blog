3c50fab64342e0c5a3195ad5503fb5f5aedf0f8d
Sanjith0524 <sanjith.m2023@vitstudent.ac.in>
2024-01-19 19:40:32 +0530

    Changes

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import Author,Post,Tag
 class PostAdmin(admin.ModelAdmin):
     list_filter = ('author','tags','Date')
     list_display = ('title','author','Date')
-    prepopulated_fields = {'Slug':('title',)}
+    prepopulated_fields = {'slug':('title',)}
 
 admin.site.register(Author)
 admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -19,11 +19,16 @@ class Author(models.Model):
 class Post(models.Model):
     title=models.CharField(max_length=50)
     excerpt=models.CharField(max_length=100)
-    Image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts",null=True)
     Date = models.DateField(auto_now=True)
-    Slug = models.SlugField(max_length=50,unique=True,db_index=True)
+    slug = models.SlugField(max_length=50,unique=True,db_index=True)
     Content  = models.TextField(validators=[MinLengthValidator(10)])
     author  = models.ForeignKey(Author,on_delete=models.SET_NULL,related_name='posts',null=True)
     tags = models.ManyToManyField(Tag)
 
 
+class Comments(models.Model):
+    user_name = models.CharField(max_length=30)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=200)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
